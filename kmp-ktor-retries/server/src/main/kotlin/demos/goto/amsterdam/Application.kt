6bd3e9dd5fd02e64b2ef1376f65a413b6ec7bbaa
Garth Gilmour <garth.gilmour@jetbrains.com>
2024-06-14 09:46:06 +0100

    Add KMP projects

---
+++ kmp-ktor-retries/server/src/main/kotlin/demos/goto/amsterdam/Application.kt
@@ -0,0 +1,25 @@
+package demos.goto.amsterdam
+
+import demos.goto.amsterdam.plugins.configureRouting
+import demos.goto.amsterdam.plugins.configureSerialization
+import io.ktor.client.engine.cio.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.koin.ktor.plugin.Koin
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    install(Koin) {
+        modules(org.koin.dsl.module {
+            single { CIO.create() }
+        })
+    }
+
+    configureSerialization()
+    configureRouting()
+}
---
+++ kmp-ktor-retries/server/src/main/kotlin/demos/goto/amsterdam/Question.kt
@@ -0,0 +1,6 @@
+package demos.goto.amsterdam
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Question(var id: Long, var title: String)
